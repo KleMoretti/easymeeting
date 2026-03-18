8809bf3da9ec28ae7e71da448220c89cce39c837
Moretti Klein <353813@whut.edu.cn>
2026-03-18 18:17:26 +0800

    Add WebSocket message handling for peer connections; implement PeerConnectionDataDto and PeerMessageDto

---
+++ src/main/java/com/easymeeting/entity/dto/PeerMessageDto.java
@@ -0,0 +1,23 @@
+package com.easymeeting.entity.dto;
+
+public class PeerMessageDto {
+
+    private String signalType;
+    private String signalData;
+
+    public String getSignalType() {
+        return signalType;
+    }
+
+    public void setSignalType(String signalType) {
+        this.signalType = signalType;
+    }
+
+    public String getSignalData() {
+        return signalData;
+    }
+
+    public void setSignalData(String signalData) {
+        this.signalData = signalData;
+    }
+}
--- src/main/java/com/easymeeting/websocket/netty/HandlerWebSocket.java
+++ src/main/java/com/easymeeting/websocket/netty/HandlerWebSocket.java
@@ -1,8 +1,19 @@
 package com.easymeeting.websocket.netty;
 
+import com.easymeeting.entity.constants.Constants;
+import com.easymeeting.entity.dto.MessageSendDto;
+import com.easymeeting.entity.dto.PeerConnectionDataDto;
+import com.easymeeting.entity.dto.PeerMessageDto;
+import com.easymeeting.entity.dto.TokenUserInfoDto;
 import com.easymeeting.entity.po.UserInfo;
 import com.easymeeting.entity.query.UserInfoQuery;
+import com.easymeeting.enums.MessageSend2TypeEnum;
+import com.easymeeting.enums.MessageTypesEnum;
 import com.easymeeting.mappers.UserInfoMapper;
+import com.easymeeting.redis.RedisComponent;
+import com.easymeeting.utils.JsonUtils;
+import com.easymeeting.websocket.message.MessageHandler;
+import com.rabbitmq.tools.json.JSONUtil;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -10,6 +21,7 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -21,6 +33,10 @@ public class HandlerWebSocket extends SimpleChannelInboundHandler<TextWebSocketF
 
     @Resource
     private UserInfoMapper<UserInfo, UserInfoQuery> userInfoMapper;
+    @Resource
+    private RedisComponent redisComponent;
+    @Resource
+    private MessageHandler messageHandler;
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -43,7 +59,31 @@ public class HandlerWebSocket extends SimpleChannelInboundHandler<TextWebSocketF
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
         String text = textWebSocketFrame.text();
-        //log.error("收到消息{}", text);
-        //TODO 处理消息的逻辑
+
+        if(Constants.PING.equals(text)){
+            return;
+        }
+        log.error("收到消息{}", text);
+        PeerConnectionDataDto dataDto= JsonUtils.convertJson2Obj(text, PeerConnectionDataDto.class);
+        TokenUserInfoDto tokenUserInfoDto=redisComponent.getTokenUserInfoDto(dataDto.getToken());
+        if(tokenUserInfoDto==null){
+            return;
+        }
+        MessageSendDto messageSendDto=new MessageSendDto();
+        messageSendDto.setMessageType(MessageTypesEnum.PEER.getType());
+        PeerMessageDto peerMessageDto=new PeerMessageDto();
+        peerMessageDto.setSignalType(dataDto.getSignalType());
+        peerMessageDto.setSignalData(dataDto.getSignalData());
+
+        messageSendDto.setMessageContent(peerMessageDto);
+
+        messageSendDto.setMeetingId(tokenUserInfoDto.getCurrentMeetingId());
+        messageSendDto.setSendUserId(tokenUserInfoDto.getUserId());
+        messageSendDto.setReceiveUserId(dataDto.getReceiveUserId());
+        messageSendDto.setMessageSend2Type(MessageSend2TypeEnum.USER.getType());
+
+        messageHandler.sendMessage(messageSendDto);
     }
+
+
 }
